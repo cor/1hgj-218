1b2c751c59fc73054bdc3ce18c3d00f4cbe80a4e
cor <cor@pruijs.nl>
2019-06-29 22:35:23 +0200

    Add laser spawners and player movement

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(10, 0);
+        rb.AddRelativeForce(new Vector2(1, 0) * 10 - rb.velocity);
+    }
+}
---
+++ Assets/Scripts/LaserSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSpawner : MonoBehaviour
+{
+    public GameObject laser;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            GameObject laserClone = Instantiate(laser, transform);
+
+            laserClone.transform.Rotate(0, 0, Random.Range(0, 360));
+        }
+    }
+}
